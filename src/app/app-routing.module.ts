f34f3354654ed28602b3ceae2e3e86533e0fea76
JoelRomeroG <joel.romero@polimentes.mx>
2019-01-24 12:10:17 -0600

    Routes for tictactoe and testing3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [{path: '', loadChildren: './tictactoe/tictactoe.module#TictactoeModule', canLoad: []}];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './tictactoe/tictactoe.module#TictactoeModule',
+    canLoad: []
+  }, {
+    path: 'test3',
+    loadChildren: './testing3/testing3.module#Testing3Module',
+    canLoad: []
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
